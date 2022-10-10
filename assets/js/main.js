0b98eed411e228a3831e16c88812a6869a7163e9
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-10-10 13:46:28 -0400

    header creation, Header css collapse header Universal

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+/**
+ * toggle active class on header
+ * when clicked nav-toggle-btn
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
